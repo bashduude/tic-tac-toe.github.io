9e6c4dcb165ec8c263a53311f14b5191398a5e47
bashduude <itsgene94@gmail.com>
2017-12-27 22:52:13 +0300

    changed identation

--- app.js
+++ app.js
@@ -15,163 +15,155 @@ $(document).ready(function() {
   // объявляю переменную победителя.
   var winner = "<h1>" + "Победил: ";
 
-  // устанавливаю эвент на детей "игровой площадки", мол "при отпускании щелчка мышки"
+  // устанавливаю эвент на детей "игровой площадки", при клике
   // выполняй функцию.
-/*
-  $('.gamefield').children().each(function() {
-    $(this).on('click', function() {
-*/
-      $('.gamefield .box').on('click', function() {
+  $('.gamefield .box').on('click', function() {
 
-      // определяю пустая ли ячейка, если пустая, выполнение выше описанной функции
-      // продолжается.
-      if ($(this).html() == "") {
+  // определяю пустая ли ячейка, если пустая, выполнение выше описанной функции
+  // продолжается.
+  if ($(this).html() == "") {
 
-        // switch statement опредяет текущее значение переменной xoro(икс или о).
-        switch (xoro) {
-          case "O":
-            xoro = "X";
-          break;
-          case "X":
-            xoro = "O";
-          break;
-        }
+    // switch statement опредяет текущее значение переменной xoro(икс или о).
+    switch (xoro) {
+    case "O":
+    xoro = "X";
+    break;
+    case "X":
+    xoro = "O";
+    break;
+    }
 
-        // определяю наличие "икс" и "о" в клетке, если их нет, функция продолжается.
-        if($(this).html() != "X" && $(this).html() != "O" && endofgame == false) {
+    // определяю наличие "икс" и "о" в клетке, если их нет, функция продолжается.
+    if($(this).html() != "X" && $(this).html() != "O" && endofgame == false) {
 
-          // ставлю крестик или нолик на поле в зависимости от текущего значения переменной.
-          $(this).html(xoro);
+    // ставлю крестик или нолик на поле в зависимости от текущего значения переменной.
+    $(this).html(xoro);
 
-          // увеличиваю количество ноликов или крестиков в переменной в зависимости
-          // от свеже-измененной клетки.
-          if(xoro == "X") {
-            xval++;
-          } else {
-            oval++;
-          }
+    // увеличиваю количество ноликов или крестиков в переменной в зависимости
+    // от свеже-измененной клетки.
+    if(xoro == "X") {
+    xval++;
+    } else {
+    oval++;
+    }
 
-          // проверка на ничью.
-          if (oval + xval == 9) {
-            $(".winner").html("<h1>" + "Ничья!" + "</h1>").addClass("endofgame");
-            $(".box").addClass("endofgame");
-          }
+    // проверка на ничью.
+    if (oval + xval == 9) {
+    $(".winner").html("<h1>" + "Ничья!" + "</h1>").addClass("endofgame");
+    $(".box").addClass("endofgame");
+    }
+    }
+  }
 
-        }
-      }
+  // добавляю на экран сумму ноликов в данный момент на поле.
+  $(".oval").html("<p>" + "Сумма O:  " + oval + "</p>");
 
-      // добавляю на экран сумму ноликов в данный момент на поле.
-      $(".oval").html("<p>" + "Сумма O:  " + oval + "</p>");
-
-      // добавляю на экран сумму крестиков в данный момент на поле.
-      $(".xval").html("<p>" + "Сумма X:  " + xval + "</p>");
+  // добавляю на экран сумму крестиков в данный момент на поле.
+  $(".xval").html("<p>" + "Сумма X:  " + xval + "</p>");
 
   if (endofgame == false) {
-  // ниже описана логика победы того или иного игрока.
+
+    // ниже описана логика победы того или иного игрока.
     if($(".field-1").html() + $(".field-2").html() + $(".field-3").html() == "XXX" ||
-     $(".field-1").html() + $(".field-2").html() + $(".field-3").html() == "OOO") {
+    $(".field-1").html() + $(".field-2").html() + $(".field-3").html() == "OOO") {
 
-        winner += xoro + "</h1>";
-        console.log(winner);
+      winner += xoro + "</h1>";
+      $(".winner").html(winner).addClass("endofgame");
+      $(".box").addClass("endofgame");
 
-        $(".winner").html(winner).addClass("endofgame");
-        $(".box").addClass("endofgame");
+    } else if ($(".field-7").html() + $(".field-8").html() + $(".field-9").html() == "XXX" ||
+    $(".field-7").html() + $(".field-8").html() + $(".field-9").html() == "OOO") {
 
-     } else if ($(".field-7").html() + $(".field-8").html() + $(".field-9").html() == "XXX" ||
-      $(".field-7").html() + $(".field-8").html() + $(".field-9").html() == "OOO") {
+      winner += xoro + "</h1>";
+      $(".winner").html(winner).addClass("endofgame");
+      $(".box").addClass("endofgame");
 
-        winner += xoro + "</h1>";
-        $(".winner").html(winner).addClass("endofgame");
-        $(".box").addClass("endofgame");
+    } else if ($(".field-1").html() + $(".field-4").html() + $(".field-7").html() == "XXX" ||
+    $(".field-1").html() + $(".field-4").html() + $(".field-7").html() == "OOO") {
 
-     } else if ($(".field-1").html() + $(".field-4").html() + $(".field-7").html() == "XXX" ||
-      $(".field-1").html() + $(".field-4").html() + $(".field-7").html() == "OOO") {
+      winner += xoro + "</h1>";
+      $(".winner").html(winner).addClass("endofgame");
+      $(".box").addClass("endofgame");
 
-        winner += xoro + "</h1>";
-        $(".winner").html(winner).addClass("endofgame");
-        $(".box").addClass("endofgame");
+    } else if ($(".field-3").html() + $(".field-6").html() + $(".field-9").html() == "XXX" ||
+    $(".field-3").html() + $(".field-6").html() + $(".field-9").html() == "OOO") {
 
-     } else if ($(".field-3").html() + $(".field-6").html() + $(".field-9").html() == "XXX" ||
-      $(".field-3").html() + $(".field-6").html() + $(".field-9").html() == "OOO") {
+      winner += xoro + "</h1>";
+      $(".winner").html(winner).addClass("endofgame");
+      $(".box").addClass("endofgame");
 
-        winner += xoro + "</h1>";
-        $(".winner").html(winner).addClass("endofgame");
-        $(".box").addClass("endofgame");
+    } else if ($(".field-1").html() + $(".field-5").html() + $(".field-9").html() == "XXX" ||
+    $(".field-1").html() + $(".field-5").html() + $(".field-9").html() == "OOO") {
 
-     } else if ($(".field-1").html() + $(".field-5").html() + $(".field-9").html() == "XXX" ||
-      $(".field-1").html() + $(".field-5").html() + $(".field-9").html() == "OOO") {
+      winner += xoro + "</h1>";
+      $(".winner").html(winner).addClass("endofgame");
+      $(".box").addClass("endofgame");
 
-        winner += xoro + "</h1>";
-        $(".winner").html(winner).addClass("endofgame");
-        $(".box").addClass("endofgame");
+    } else if ($(".field-7").html() + $(".field-5").html() + $(".field-3").html() == "XXX" ||
+    $(".field-7").html() + $(".field-5").html() + $(".field-3").html() == "OOO") {
 
-     } else if ($(".field-7").html() + $(".field-5").html() + $(".field-3").html() == "XXX" ||
-      $(".field-7").html() + $(".field-5").html() + $(".field-3").html() == "OOO") {
+      winner += xoro + "</h1>";
+      $(".winner").html(winner).addClass("endofgame");
+      $(".box").addClass("endofgame");
 
-        winner += xoro + "</h1>";
-        $(".winner").html(winner).addClass("endofgame");
-        $(".box").addClass("endofgame");
+    } else if ($(".field-4").html() + $(".field-5").html() + $(".field-6").html() == "XXX" ||
+    $(".field-4").html() + $(".field-5").html() + $(".field-6").html() == "OOO") {
 
-     } else if ($(".field-4").html() + $(".field-5").html() + $(".field-6").html() == "XXX" ||
-      $(".field-4").html() + $(".field-5").html() + $(".field-6").html() == "OOO") {
+      winner += xoro + "</h1>";
+      $(".winner").html(winner).addClass("endofgame");
+      $(".box").addClass("endofgame");
 
-        winner += xoro + "</h1>";
-        $(".winner").html(winner).addClass("endofgame");
-        $(".box").addClass("endofgame");
+    } else if ($(".field-2").html() + $(".field-5").html() + $(".field-8").html() == "XXX" ||
+    $(".field-2").html() + $(".field-5").html() + $(".field-8").html() == "OOO") {
 
-     } else if ($(".field-2").html() + $(".field-5").html() + $(".field-8").html() == "XXX" ||
-      $(".field-2").html() + $(".field-5").html() + $(".field-8").html() == "OOO") {
+      winner += xoro + "</h1>";
+      $(".winner").html(winner).addClass("endofgame");
+      $(".box").addClass("endofgame");
 
-        winner += xoro + "</h1>";
-        $(".winner").html(winner).addClass("endofgame");
-        $(".box").addClass("endofgame");
+    }
+  }
 
-     }
-   }
+  // проверка на победу.
+  if($(".winner").hasClass("endofgame")) {
+    $(".btn-class").html("<button class='clicktorestart'>" + "Заново?" + "</button>");
 
-     // проверка на победу.
-     if($(".winner").hasClass("endofgame")) {
-       $(".btn-class").html("<button class='clicktorestart'>" + "Заново?" + "</button>");
+    // если победил или ничья - игра закончилась.
+    endofgame = true;
 
-       // если победил или ничья - игра закончилась.
-       endofgame = true;
+  }
 
-     }
+  // если игра закончилась, -
+  if (endofgame == true) {
+    $(".clicktorestart").on('click', function() {
 
-     // если игра закончилась, -
-     if (endofgame == true) {
-       $(".clicktorestart").on('click', function() {
+      // сбрасываю класс конец игры с элементов.
+      $(".winner").removeClass("endofgame");
+      $(".box").removeClass("endofgame");
 
-         // сбрасываю класс конец игры с элементов.
-         $(".winner").removeClass("endofgame");
-         $(".box").removeClass("endofgame");
+      // "перезапускаю" конец игры, делаю ее снова "незаконченой".
+      endofgame = false;
 
-         // "перезапускаю" конец игры, делаю ее снова "незаконченой".
-         endofgame = false;
+      // очищаю поле от крестиков и ноликов.
+      $('.gamefield').children().each(function() {
+        $(this).html("");
+      });
 
-        // очищаю поле от крестиков и ноликов.
-         $('.gamefield').children().each(function() {
-           $(this).html("");
-         });
+      // убираю результат о победителе с экрана.
+      $(".winner").html("");
 
-         // убираю результат о победителе с экрана.
-         $(".winner").html("");
+      // убираю результат о победителе с переменной.
+      winner = "<h1>" + "Победил: ";
 
-         // убираю результат о победителе с переменной.
-         winner = "<h1>" + "Победил: ";
+      // сбрасываю значение количества Х и О на экране и в счетчике.
+      oval = 0;
+      xval = 0;
+      $(".oval").html("<p>" + "Сумма O:  " + oval + "</p>");
+      $(".xval").html("<p>" + "Сумма X:  " + xval + "</p>");
 
-         // сбрасываю значение количества Х и О на экране и в счетчике.
-         oval = 0;
-         xval = 0;
-         $(".oval").html("<p>" + "Сумма O:  " + oval + "</p>");
-         $(".xval").html("<p>" + "Сумма X:  " + xval + "</p>");
-
-         // убираю кнопку, после нажатия
-         $(".clicktorestart").remove();
-
-       });
-     }
+      // убираю кнопку, после нажатия
+      $(".clicktorestart").remove();
 
     });
-  //});
+  }
+  });
 });

0c4ca644e1e7f09a12a0ee44716b67d712887f9a
bashduude <itsgene94@gmail.com>
2017-12-27 13:01:07 +0300

    now you can click on each box and get x or o depending on previous click, first click is always o

--- app.js
+++ app.js
@@ -1,1 +1,140 @@
-alert("im here!");
+$(document).ready(function() {
+
+var value;
+
+var xoro = "O";
+
+var pls;
+
+
+
+  $(".field-1").click(function () {
+      pls = xoro;
+    if (xoro != "X") {
+      xoro = "X";
+    } else {
+      xoro = "O";
+    }
+
+    value = "";
+    value += "<p class='xando'>" + pls + "</p>";
+    $(".field-1").html(value);
+  });
+
+  $(".field-2").click(function () {
+      pls = xoro;
+    if (xoro != "X") {
+      xoro = "X";
+    } else {
+      xoro = "O";
+    }
+
+    value = "";
+    value += "<p class='xando'>" + pls + "</p>";
+    $(".field-2").html(value);
+  });
+
+  $(".field-3").click(function () {
+      pls = xoro;
+    if (xoro != "X") {
+      xoro = "X";
+    } else {
+      xoro = "O";
+    }
+
+    value = "";
+    value += "<p class='xando'>" + pls + "</p>";
+    $(".field-3").html(value);
+  });
+
+  $(".field-4").click(function () {
+      pls = xoro;
+    if (xoro != "X") {
+      xoro = "X";
+    } else {
+      xoro = "O";
+    }
+
+    value = "";
+    value += "<p class='xando'>" + pls + "</p>";
+    $(".field-4").html(value);
+  });
+
+  $(".field-5").click(function () {
+      pls = xoro;
+    if (xoro != "X") {
+      xoro = "X";
+    } else {
+      xoro = "O";
+    }
+
+    value = "";
+    value += "<p class='xando'>" + pls + "</p>";
+    $(".field-5").html(value);
+  });
+
+  $(".field-6").click(function () {
+      pls = xoro;
+    if (xoro != "X") {
+      xoro = "X";
+    } else {
+      xoro = "O";
+    }
+
+    value = "";
+    value += "<p class='xando'>" + pls + "</p>";
+    $(".field-6").html(value);
+  });
+
+  $(".field-7").click(function () {
+      pls = xoro;
+    if (xoro != "X") {
+      xoro = "X";
+    } else {
+      xoro = "O";
+    }
+
+    value = "";
+    value += "<p class='xando'>" + pls + "</p>";
+    $(".field-7").html(value);
+  });
+
+  $(".field-8").click(function () {
+      pls = xoro;
+    if (xoro != "X") {
+      xoro = "X";
+    } else {
+      xoro = "O";
+    }
+
+    value = "";
+    value += "<p class='xando'>" + pls + "</p>";
+    $(".field-8").html(value);
+  });
+
+
+
+  $(".field-9").click(function () {
+      pls = xoro;
+    if (xoro != "X") {
+      xoro = "X";
+    } else {
+      xoro = "O";
+    }
+
+
+    value = "";
+    value += "<p class='xando'>" + pls + "</p>";
+    $(".field-9").html(value);
+  });
+
+
+/*
+
+  if ((".field-1").val() + (".field-2").val() + (".field-1").val()) {
+
+  }
+*/
+
+
+});

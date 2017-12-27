b54dd3fcf521edb417898a7acc97d9cd3bf93ba3
bashduude <itsgene94@gmail.com>
2017-12-27 15:09:46 +0300

    App now has neat shorthand function to put X and O in the boxes, alse it is checking if the box is empty or if it has X or O in it, to make a ddesicion about placing another X or O

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 $(document).ready(function() {
 
-var value;
 var xoro = "O";
 
-value = "";
-//value += "<p class='xando'>" + xoro + "</p>";
-
-/*
 
 $('.gamefield').children().each(function() {
   $(this).on('mouseup', function() {
-    if ($(this).val() != "X" || $(this).val() != "O" || $(this).val() != "") {
+    if ($(this).html() == "") {
+
     switch (xoro) {
       case "O":
         xoro = "X";
@@ -21,74 +17,56 @@ $('.gamefield').children().each(function() {
       default:
     }
 
-    value += "<p class='xando'>" + xoro + "</p>";
-    $(this).html(xoro);
-  }
+    if($(this).html() != "X" && $(this).html() != "O") {
+      $(this).html(xoro);
+    }
+    }
   });
 });
 
-*/
+
+
+/*
 
 $('.box').mouseup(function() {
   var oloa;
 
-  /*
-  $(this).on('mouseup', function() {
-    if ($(this).val() != "X" || $(this).val() != "O" || $(this).val() != "") {
-    switch (xoro) {
-      case "O":
-        xoro = "X";
-      break;
-      case "X":
-        xoro = "O";
-      break;
-      default:
-    }
-
-    value += "<p class='xando'>" + xoro + "</p>";
-    $(this).html(xoro);
-  }
-  });
-
-  */
 
   switch ($(".box").index(this)) {
     case 0:
       oloa = "";
-      alert("Ячейка номер: 1");
+      $(".field-1").html(value);
+
       break;
     case 1:
-      alert("Ячейка номер: 2");
+
       break;
     case 2:
-      alert("Ячейка номер: 3");
+
       break;
     case 3:
-      alert("Ячейка номер: 4");
+
       break;
     case 4:
-      alert("Ячейка номер: 5");
+
       break;
     case 5:
-      alert("Ячейка номер: 6");
+
       break;
     case 6:
-      alert("Ячейка номер: 7");
+
       break;
     case 7:
-      alert("Ячейка номер: 8");
+
       break;
     case 8:
-      alert("Ячейка номер: 9");
+
       break;
   }
 
-
-
-
-
 });
 
+*/
 
 
 
